d39249c093eee08d423bafba3b54d8c840b49429
Cyrusublerman <Einoder5@gmail.com>
2026-07-19 14:17:47 +1000

    Add portable project lifecycle

--- src/pydesign/runtime/__init__.py
+++ src/pydesign/runtime/__init__.py
@@ -6,13 +6,33 @@ in a disposable subprocess.
 
 from pydesign.runtime.client import EvaluationResult, WorkerClient
 from pydesign.runtime.project import ProjectConfig, load_project_config
+from pydesign.runtime.project_files import (
+    PackageResult,
+    ProjectOperationError,
+    UnsafeProjectLocationError,
+    create_project,
+    duplicate_project,
+    ensure_safe_project_destination,
+    find_pydesign_source_checkout,
+    is_bundled_example,
+    package_project,
+)
 from pydesign.runtime.recovery import RecoverySnapshot, RecoveryStore
 
 __all__ = [
     "EvaluationResult",
+    "PackageResult",
     "ProjectConfig",
+    "ProjectOperationError",
     "RecoverySnapshot",
     "RecoveryStore",
     "WorkerClient",
+    "UnsafeProjectLocationError",
+    "create_project",
+    "duplicate_project",
+    "ensure_safe_project_destination",
+    "find_pydesign_source_checkout",
+    "is_bundled_example",
     "load_project_config",
+    "package_project",
 ]
